7443355b521ac3a34be5795d4f60c9d98d6cc1b8
WanderedToLa <rladntjr9808@gmail.com>
2022-12-13 10:35:50 +0900

    Update : algorithm test

---
+++ __test__/ATM.test.ts
@@ -0,0 +1,16 @@
+let sum = 0;
+let time = 0;
+
+function ATM(...p) {
+  const arr = [...p];
+  arr.sort();
+  for (let i = 0; i <= arr.length; i++) {
+    sum += i;
+    time += sum;
+  }
+  return time;
+}
+
+test("오름차순으로 정렬 후 누적 합 계산", () => {
+  expect(ATM(3, 1, 2, 5, 4)).toStrictEqual(35);
+});
